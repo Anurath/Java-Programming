9181adf135813bb7593cf35ce505f3b0e8680b69
anurath <anurathwaghmode2018@gmail.com>
2025-03-03 12:57:21 +0530

    new method file added

---
+++ Methods/isKrishnMurtiNumber.java
@@ -0,0 +1,31 @@
+class RangeOfKrishnMurthiNumber
+{
+	public static void main(String[] args)
+	{
+		for (int i=1;i<10000;i++ )
+		{
+			if (isKrishnMurtiNumber(i))
+			{
+				System.out.println(i);
+			}
+		}
+	}
+	public static boolean isKrishnMurtiNumber(int num)
+	{
+		int sum =0;
+		for (int i=num;i!=0;i/=10 )
+		{
+			sum+=(factorial((i%10)));
+		}
+		return sum==num;
+	}
+	public static int factorial(int num)
+	{
+		int fact=1;
+		for (int i=num;i>0;i-- )
+		{
+			fact*=(i);
+		}
+		return fact;
+	}
+}
