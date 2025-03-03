ce2ffc6316b18d344aacb2caac59216f77c7a52b
anurath <anurathwaghmode2018@gmail.com>
2025-03-03 13:03:19 +0530

    range of prime file added

---
+++ Methods/RangeOfPrimeNumber.java
@@ -0,0 +1,23 @@
+class RangeOfPrimeNumber
+{
+	public static void main(String[] args)
+	{
+		for (int i=1;i<100 ;i++ )
+		{
+			if(isPrime(i))
+				System.out.println(i);
+		}
+	}
+	public static boolean isPrime(int num)
+	{
+		int den=2;
+		for (;den<num ;den++ )
+		{
+			if(num%den==0)
+			{
+				return false;
+			}
+		}
+		return den==num;
+	}
+}
