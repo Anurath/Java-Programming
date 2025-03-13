c50f1e30c0b1f93bf63c9e171f19fe759e15ae5f
anurath <anurathwaghmode2018@gmail.com>
2025-03-13 22:23:25 +0530

    math new problem added

---
+++ Methods/MakeMaxNumber.java
@@ -0,0 +1,46 @@
+class  MakeMaxNumber
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(maxNum(1));
+	}
+	public static int maxNum(int num)
+	{
+		int maxNum =0 ,minNum=9,cnt=0;
+        String str="";
+        for(int i=num;i!=0;i/=10)
+        {
+            int rem = i%10;
+            if(rem>maxNum)
+            {
+                maxNum=rem;
+            }
+            else if (rem<minNum)
+            {
+                minNum=rem;
+            }
+			cnt++;
+        }
+		if(cnt==1)
+		{
+			return num;
+		}
+        for(int i=num;i!=0;i/=10)
+        {
+            if(i%10==minNum)
+            {
+                str=maxNum+str;
+            }
+            else if(i%10==maxNum)
+            {
+                str=minNum+str;
+            }
+			else
+			{
+				str=(i%10)+str;
+			}
+        }
+		int op=Integer.parseInt(str);
+		return op;
+	}
+}
