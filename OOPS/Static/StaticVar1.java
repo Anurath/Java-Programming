88b6836c8682b06d0d157e56054c2cddeb957b04
anurath <anurathwaghmode2018@gmail.com>
2025-03-22 13:17:37 +0530

    static keyword files addded for variable

--- OOPS/Static/StaticVar1.java
+++ OOPS/Static/StaticVar1.java
@@ -3,6 +3,8 @@ class StaticVar1
 	static int num;
 	public static void main(String[] args)
 	{
+		int num=1000;
 		System.out.println(num);
+		System.out.println(StaticVar1.num);
 	}
 }
---
+++ OOPS/Static/StaticVar2.java
@@ -0,0 +1,8 @@
+class StaticVar2 
+{
+	public static void main(String[] args) 
+	{
+		int num=StaticVar1.num;
+		System.out.println(num);
+	}
+}
