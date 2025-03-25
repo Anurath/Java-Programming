96c514dc63e86008986d34ffe34c1102da16227e
anurath <anurathwaghmode2018@gmail.com>
2025-03-25 10:57:34 +0530

    Public class Files added

---
+++ OOPS/Public Class/Demo.java
@@ -0,0 +1,23 @@
+class Test1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("From Test2");
+	}
+}
+class Test2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("From Test2");
+	}
+}
+
+public class Demo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("From Public Demo");
+	}
+}
+
