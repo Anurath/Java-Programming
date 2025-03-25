2614ef330f055475738b8d17f01b3270618c41d1
anurath <anurathwaghmode2018@gmail.com>
2025-03-25 10:56:33 +0530

    Multiclass Files added

---
+++ OOPS/Multiclass/XYZ.java
@@ -0,0 +1,21 @@
+class A
+{
+	public static void main(String[] args)
+	{
+		System.out.println("From A");
+	}
+}
+class B
+{
+	public static void main(String[] args)
+	{
+		System.out.println("From B");
+	}
+}
+class C
+{
+	public static void main(String[] args)
+	{
+		System.out.println("From B");
+	}
+}
