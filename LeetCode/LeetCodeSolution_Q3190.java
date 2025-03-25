4d1be6371313faf99b1b41640ce2a2a304040620
anurath <anurathwaghmode2018@gmail.com>
2025-03-26 01:41:31 +0530

    Leet code solution Q.3190

---
+++ LeetCode/LeetCodeSolution_Q3190.java
@@ -0,0 +1,13 @@
+class LeetCodeSolution_Q3190{
+    public int minimumOperations(int[] nums) {
+        byte cnt=0;
+		for (byte i=0;i<nums.length;i++)
+		{
+			if (nums[i]%3!=0)
+			{
+				cnt++;
+			}
+		}
+		return cnt;
+    }
+}
