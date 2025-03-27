8bbd07f45afc9318e34624350b4d2fc8c310ddb4
anurath <anurathwaghmode2018@gmail.com>
2025-03-28 00:40:19 +0530

    this keyword files added

---
+++ OOPS/this Keyword/Student.java
@@ -0,0 +1,15 @@
+class Student
+{
+	String name;
+	double score;
+	int age;
+
+	public void displayStudent()
+	{
+		String name="Tinku";
+		System.out.println("Name of Student: "+this.name);
+		System.out.println("Nickname of Student :"+name);
+		System.out.println("Score ofStudent: "+score);
+		System.out.println("Age of Student: "+age);
+	}
+}
---
+++ OOPS/this Keyword/StudentDriver.java
@@ -0,0 +1,11 @@
+class StudentDriver
+{
+	public static void main(String[] args)
+	{
+		Student s1=new Student();
+		s1.name="Ram";
+		s1.age=22;
+		s1.score=9.32;
+		s1.displayStudent();
+	}
+}
