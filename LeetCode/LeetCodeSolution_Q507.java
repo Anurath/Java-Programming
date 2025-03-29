c97afce9199914d4661f75b249362a9b5be19769
anurath <anurathwaghmode2018@gmail.com>
2025-03-30 01:17:46 +0530

    Leet code solution Q.507

---
+++ LeetCode/LeetCodeSolution_Q507.java
@@ -0,0 +1,13 @@
+class LeetCodeSolution_Q507{
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<=num/2;i++)
+        {
+            if(num%i==0)
+            {
+                sum+=i;
+            }
+        }
+        return num==sum;
+    }
+}
