57d0d91d94ac1a4b04286328949f2014c3830f60
anurath <anurathwaghmode2018@gmail.com>
2025-03-30 01:18:35 +0530

    Leet code solution Q.367

---
+++ LeetCode/LeetCodeSolution_Q367.java
@@ -0,0 +1,22 @@
+class LeetCodeSolution_Q367{
+    public boolean isPerfectSquare(int num) {
+        boolean flag=false;
+        if(num==1)
+        {
+            return true;
+        }
+        for(int i=1;i<=num/2;i++)
+        {
+            if(i*i==num)
+            {
+                flag = true;
+                break;
+            }
+            if(i*i>num)
+            {
+                break;
+            }
+        }
+        return flag;
+    }
+}
