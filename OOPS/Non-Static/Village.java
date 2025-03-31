84b8facf004abad656ee8366856f1cdd273897cf
anurath <anurathwaghmode2018@gmail.com>
2025-04-01 00:08:35 +0530

    Village File added in non static concept

---
+++ OOPS/Non-Static/Village.java
@@ -0,0 +1,19 @@
+class Village
+{
+	String name;
+	int population;
+	int male;
+	int female;
+	String sarpanch;
+	String nearByVillage;
+	public void displayVillage()
+	{
+		String name="Gaav";
+		System.out.println("Name of Village: "+this.name);
+		System.out.println("Population of Village: "+population);
+		System.out.println("Male Population: "+male);
+		System.out.println("Female Population: "+female);
+		System.out.println("Sarpanch: "+sarpanch);
+		System.out.println("Near By Village: "+nearByVillage);
+	}
+}
---
+++ OOPS/Non-Static/VillageDriver.java
@@ -0,0 +1,13 @@
+class VillageDriver
+{
+	public static void main(String[] args)
+	{
+		Village v1=new Village();
+		v1.name="Gilgaon";
+		v1.population=300;
+		v1.male=170;
+		v1.female=130;
+		v1.sarpanch="Vitthal Waghmode";
+		v1.displayVillage();
+	}
+}
