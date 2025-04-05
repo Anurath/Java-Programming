2ef456f90e8225a87943ef3c64fb89b12a1a6fa4
anurath <anurathwaghmode2018@gmail.com>
2025-04-05 11:01:08 +0530

    Leetcode solution Q.326 added

---
+++ LeetCode/LeetCodeSolution_Q326.java
@@ -0,0 +1,10 @@
+class LeetCodeSolution_Q326{
+    public boolean isPowerOfThree(int n) {
+        if(n<=0)
+            return false;
+        for(;n!=1;n/=3)
+            if(n%3!=0)
+				return false;
+        return n!=1?false:true;
+    }
+}
