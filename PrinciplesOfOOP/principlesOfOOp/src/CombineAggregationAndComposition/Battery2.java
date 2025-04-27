e72900ff878c63f708a7c3e67983374821a4384b
anurath <anurathwaghmode2018@gmail.com>
2025-04-27 23:09:45 +0530

    Mobile product with private data members

--- PrinciplesOfOOP/principlesOfOOp/src/CombineAggregationAndComposition/Battery2.java
+++ PrinciplesOfOOP/principlesOfOOp/src/CombineAggregationAndComposition/Battery2.java
@@ -1,5 +1,23 @@
 package CombineAggregationAndComposition;
 
 public class Battery2 {
-
+	private String brand;
+	private double price;
+	private int hour;
+	private double power;
+	
+	Battery2(){}
+	
+	Battery2(String brand,double price,int hour,double power)
+	{
+		this.brand = brand;
+		this.power = power;
+		this.price = price;
+		this.hour = hour;
+	}
+	
+	public void displayBattery()
+	{
+		System.out.println("Battery: [ Brand: +"+brand+" , Price: "+price+" , Hour: "+hour+" , Power: "+power);
+	}
 }
