42ce67be072973a44e4794708caf0ea566090f4a
anurath <anurathwaghmode2018@gmail.com>
2025-05-08 14:10:48 +0530

    relatoinship files created

---
+++ PrinciplesOfOOP/principlesOfOOp/src/CombineAggregationAndComposition/PrimeMinister.java
@@ -0,0 +1,16 @@
+package CombineAggregationAndComposition;
+
+public class PrimeMinister {
+	String name;
+	int age;
+	String party;
+	
+	PrimeMinister(){}
+	
+	PrimeMinister(String name, int age, String party)
+	{
+		this.name= name;
+		this.age= age;
+		this.party = party;
+	}
+}
