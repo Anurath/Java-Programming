5c824e3554affe8861aea2209535e9f0793af8b3
anurath <anurathwaghmode2018@gmail.com>
2025-05-08 14:19:31 +0530

    relatoinship files created

--- PrinciplesOfOOP/principlesOfOOp/src/CombineAggregationAndComposition/Country.java
+++ PrinciplesOfOOP/principlesOfOOp/src/CombineAggregationAndComposition/Country.java
@@ -1,5 +1,35 @@
 package CombineAggregationAndComposition;
 
 public class Country {
-
+	String name;
+	int independance;
+	double area;
+	long population;
+	int states;
+	long military;
+	String idiology;
+	PrimeMinister p;
+	Country(){}
+	
+	Country(String name, int independance, double area, long population,int states, long military,String idiology)
+	{
+		this.name = name;
+		this.idiology= idiology;
+		this.independance = independance;
+		this.area =area;
+		this.population = population;
+		this.states = states;
+		this.military = military;
+	}
+	public  void displayCountry()
+	{
+		System.out.println("--------------Country Inforamation------------------");
+		System.out.println("Name: "+name);
+		System.out.println("Independance: "+independance);
+		System.out.println("Area: "+area);
+		System.out.println("Population: "+population);
+		System.out.println("States: "+states);
+		System.out.println("Military: "+military);
+		System.out.println("Idiology: "+idiology);
+	}
 }
