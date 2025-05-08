f6c9057aa52d374381f6933141130118a572c894
anurath <anurathwaghmode2018@gmail.com>
2025-05-08 14:24:36 +0530

    CountryDriver file added

---
+++ PrinciplesOfOOP/principlesOfOOp/src/CombineAggregationAndComposition/CountryDriver.java
@@ -0,0 +1,9 @@
+package CombineAggregationAndComposition;
+
+public class CountryDriver {
+	public static void main(String[] args)
+	{
+		Country c1 = new Country("India",1947,329898324,1500000000,28,2100000,"Democracy");
+		c1.displayCountry();
+	}
+}
