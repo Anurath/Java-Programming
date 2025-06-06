0ff8dab0bff9b0b6df652a75364fa660ddaad8af
anurath <anurathwaghmode2018@gmail.com>
2025-06-07 00:35:33 +0530

    Hierarchical Inheritance Files Added

--- PrinciplesOfOOP/principlesOfOOp/src/inheritance/DoctorDriver.java
+++ PrinciplesOfOOP/principlesOfOOp/src/inheritance/DoctorDriver.java
@@ -5,10 +5,12 @@ public class DoctorDriver {
 	{
 		Cardiologist c1 = new Cardiologist("Pradeep","Susan","MBBS",4,"Cardio","Heart");
 		c1.displayDoctor();
+		System.out.println(".............");
 		c1.displayCardiologist();
 		System.out.println("----------------------------------");
 		Neurologist n1 = new Neurologist("Satish","Susan","MBBS",3,"Neurology","Brain");
 		n1.displayDoctor();
+		System.out.println(".............");
 		n1.displayNeurologist();
 	}
 }
