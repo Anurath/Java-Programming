c1abf36b5c500dcb7471e9ea05066161b55250d4
anurath <anurathwaghmode2018@gmail.com>
2025-06-06 14:32:35 +0530

    Multi-level inheritance files added

---
+++ PrinciplesOfOOP/principlesOfOOp/src/inheritance/Deparment.java
@@ -0,0 +1,38 @@
+package inheritance;
+
+public class Deparment extends College{
+	String deparment;
+	int stdCount;
+	String HOD;
+	int classes;
+	
+	Deparment(){}
+	
+	Deparment(String collegeName, int clgYear, int student, String principal,String uniName,int year,int collegeCnt,String vc,String chancellor,String deparment,int stdCount,String HOD,int classes)
+	{
+		this.collegeCnt = collegeCnt;
+		this.uniName = uniName;
+		this.year = year;
+		this.chancellor = chancellor;
+		this.vc = vc;
+		
+		this.collegeName = collegeName;
+		this.clgYear = clgYear;
+		this.student = student;
+		this.principal = principal;
+		
+		this.deparment = deparment;
+		this.stdCount = stdCount;
+		this.HOD = HOD;
+		this.classes = classes;
+	}
+	
+	public void displayDeparment()
+	{
+		displayCollege();
+		System.out.println("Deparment: "+deparment);
+		System.out.println("Student Count: "+stdCount);
+		System.out.println("Head Of Deparment: "+HOD);
+		System.out.println("Classes: "+classes);
+	}
+}
---
+++ PrinciplesOfOOP/principlesOfOOp/src/inheritance/UniversityDriver.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class UniversityDriver {
+	public static void main(String[] args)
+	{
+		Deparment d1 = new Deparment("P.K.Technical Campus Chakan",2011,1000,"C.V.Prem","SPPU",1949,134,"Dr.Suresh Gosavi","C.P.Radhakrushnan","Computer",180,"Abc Mam",3);
+		d1.displayDeparment();
+	}
+}
