5644609fc79401ae711f0c412b892cf1d26cd955
anurath <anurathwaghmode2018@gmail.com>
2025-06-08 22:52:37 +0530

    New Package is created for super call statement

---
+++ PrinciplesOfOOP/principlesOfOOp/src/superCallStatement/University.java
@@ -0,0 +1,32 @@
+package superCallStatement;
+
+public class University {
+	String uniName;
+	long uniStd;
+	String vc;
+	int dpCount;
+	int year;
+	String type;
+	
+	University(){}
+	
+	University(String uniName, long uniStd,String vc,int dpCount,int year,String type)
+	{
+		this.uniName = uniName;
+		this.uniStd = uniStd;
+		this.vc = vc;
+		this.dpCount = dpCount;
+		this.year = year;
+		this.type = type;
+	}
+	
+	public void displayUniversity()
+	{
+		System.out.println("University name: "+uniName);
+		System.out.println("Student Count: "+uniStd);
+		System.out.println("Vice Chancellor: "+vc);
+		System.out.println("Department Count: "+dpCount);
+		System.out.println("Year Of Istablishment: "+year);
+		System.out.println("Type Of University: "+type);
+	}
+}
