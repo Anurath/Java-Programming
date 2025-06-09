ccb8781854a635753719f5448dbaa0b90c35faee
anurath <anurathwaghmode2018@gmail.com>
2025-06-09 13:46:54 +0530

    multi-level inheritance using super call statement

--- PrinciplesOfOOP/principlesOfOOp/src/superCallStatement/Weapon.java
+++ PrinciplesOfOOP/principlesOfOOp/src/superCallStatement/Weapon.java
@@ -1,5 +1,23 @@
 package superCallStatement;
 
 public class Weapon {
-
+	int range;
+	int weight;
+	String manufacturer;
+	
+	Weapon(){}
+	
+	Weapon(int range,int weight,String manufacturer)
+	{
+		this.range = range;
+		this.weight = weight;
+		this.manufacturer = manufacturer;
+	}
+	
+	public void displayWeapon()
+	{
+		System.out.println("Range: "+range);
+		System.out.println("Weight: "+weight);
+		System.out.println("Manufacturer: "+manufacturer);
+	}
 }
---
+++ PrinciplesOfOOP/principlesOfOOp/src/superCallStatement/WeaponDriver.java
@@ -0,0 +1,9 @@
+package superCallStatement;
+
+public class WeaponDriver {
+	public static void main(String[] args)
+	{
+		CruiseMissile c1 = new CruiseMissile(500,5000,"INDIA","Bhramos","Heat","Both","Triple","0/1");
+		c1.displayCruiseMissile();
+	}
+}
