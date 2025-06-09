17bbac586ac3f250a103f21d1bb6bc3b0f3cd235
anurath <anurathwaghmode2018@gmail.com>
2025-06-09 22:31:28 +0530

    File to achive static member inheritance

---
+++ PrinciplesOfOOP/principlesOfOOp/src/inheritance/Demo.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Demo {
+	static int a=100;
+	
+	public static void main(String[] args)
+	{
+		System.out.println("From Demo main method");
+	}
+	
+	public static void test()
+	{
+		System.out.println("From test");
+	}
+	
+}
---
+++ PrinciplesOfOOP/principlesOfOOp/src/inheritance/Demo1.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Demo1 extends Demo {
+
+	public void displayDemo1()
+	{
+		System.out.println(a);
+	}
+}
