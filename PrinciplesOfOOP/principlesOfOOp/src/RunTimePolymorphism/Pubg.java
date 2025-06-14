2efc21828b2508146a883c363575aa7f474e24fb
anurath <anurathwaghmode2018@gmail.com>
2025-06-14 12:08:13 +0530

    files created for run time polymorphism

--- PrinciplesOfOOP/principlesOfOOp/src/RunTimePolymorphism/Pubg.java
+++ PrinciplesOfOOP/principlesOfOOp/src/RunTimePolymorphism/Pubg.java
@@ -1,5 +1,40 @@
 package RunTimePolymorphism;
 
 public class Pubg {
-
+	int bullets = 30;
+	public void turnLeft()
+	{
+		System.out.println("Turned Left");
+	}
+	
+	public void turnRight()
+	{
+		System.out.println("Turner Left");
+	}
+	
+	public void fire()
+	{
+		if(bullets>0)
+		{
+			bullets--;
+			System.out.println("Fired!");
+			System.out.println("Bullets Left "+bullets);
+		}
+		else
+		{
+			System.out.println("Reload.");
+		}
+	}
+	
+	public void reload()
+	{
+		bullets= 30;
+		System.out.println("Reloaded");
+	}
+	
+	public void stepForward(int steps)
+	{
+		System.out.println("Went Forward "+steps);
+	}
+	
 }
