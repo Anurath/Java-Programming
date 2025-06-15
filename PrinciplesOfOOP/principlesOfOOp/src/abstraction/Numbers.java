76c37c226ee9790263220d03e846fc27502d70f3
anurath <anurathwaghmode2018@gmail.com>
2025-06-15 23:59:05 +0530

    new files is created to achive abstraction

---
+++ PrinciplesOfOOP/principlesOfOOp/src/abstraction/Numbers.java
@@ -0,0 +1,21 @@
+package abstraction;
+
+public abstract class Numbers {
+	public abstract int nextPrime(int num);
+	
+	public abstract boolean isPrime(int num);
+	
+	public abstract int reverseNumber(int num);
+	
+	public abstract boolean petersonNumber(int num);
+	
+	public abstract boolean sunnyNumber(int num);
+	
+	public abstract boolean techNumber(int num);
+	
+	public abstract boolean neonNumber(int num);
+	
+	public abstract boolean spyNumber(int num);
+	
+	public abstract boolean emirp(int num);
+}
