45a2d8ae4f8875cb578a4810f417add08f3a0e22
anurath <anurathwaghmode2018@gmail.com>
2025-06-16 00:27:47 +0530

    Design of concrete methods in the implemention file is done

--- PrinciplesOfOOP/principlesOfOOp/src/abstraction/NumbersImp.java
+++ PrinciplesOfOOP/principlesOfOOp/src/abstraction/NumbersImp.java
@@ -40,7 +40,6 @@ public class NumbersImp extends Numbers {
 	public int reverseNumber(int num)
 	{
 		int rev = 0;
-		int dep = num;
 		
 		while(num!=0)
 		{
@@ -48,10 +47,92 @@ public class NumbersImp extends Numbers {
 			num/=10;
 		}
 		
-		return dep = rev;
+		return  rev;
 	}
 	
-	public
+	public boolean petersonNumber(int num)
+	{
+		int res = 0;
+		int dep = num;
+		
+		while(num!=0)
+		{
+			res+=factor(num%10);
+			num/=10;
+		}
+		return res ==dep;
+	}
 	
+	public int factor(int num)
+	{
+		int fact = 0;
+		for(int i=1;i<=num;i++)
+		{
+			fact*=i;
+		}
+		return fact;
+	}
 	
+	public boolean sunnyNumber(int num)
+	{
+		num+=1;
+		boolean flag = false;
+		for(int i=1;i<num;i++)
+		{
+			if(i*i==num)
+			{
+				flag = true;
+				break;
+			}
+		}
+		return flag;
+	}
+	
+	public boolean techNumber(int num)
+	{
+		int fh = num/100;
+		int lh = num%100;
+		
+		int sum  = fh + lh;
+		return sum*sum==num;
+	}
+	
+	public boolean neonNumber(int num)
+	{
+		int sum = 0;
+		int sqr  = num*num;
+		while(sqr!=0)
+		{
+			sum+=num%10;
+			sqr/=10;
+		}
+		
+		return num == sum;
+	}
+	
+	public boolean spyNumber(int num)
+	{
+		int mult = 1;
+		int sum = 0;
+		
+		while(num!=0)
+		{
+			mult*=num%10;
+			sum+=num%10;
+			num/=10;
+		}
+		return mult == sum;
+	}
+	
+	public boolean emirp(int num)
+	{
+		if(isPrime(num))
+		{
+			if(isPrime(reverseNumber(num)))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 }
