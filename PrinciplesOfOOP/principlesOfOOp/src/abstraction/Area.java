46da55251f53ce7343105089946a3d564c9c6b81
anurath <anurathwaghmode2018@gmail.com>
2025-06-15 21:31:53 +0530

    files added for abstraction

---
+++ PrinciplesOfOOP/principlesOfOOp/src/abstraction/Area.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public abstract class Area {
+	public abstract int addition(int x,int y);
+	
+	public abstract int subtraction(int x,int y);
+	
+	public abstract int multiplication(int x,int y);
+	
+	public 
+}
