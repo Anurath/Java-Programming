406dfc6bd0803b26ccf59593398f19a3a349f458
anurath <anurathwaghmode2018@gmail.com>
2025-06-16 11:16:54 +0530

    New file for abstraction is created for perimeter

---
+++ PrinciplesOfOOP/principlesOfOOp/src/abstraction/Perimeter.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public abstract class Perimeter {
+	public abstract double ractangle(int l,int b);
+	
+	public abstract double triangle(int s1,int s2,int s3);
+	
+	public abstract double square(int s);
+	
+	public abstract circle(int r);
+}
