5956fab410c40190b622ff9da2bd4d05d222b9b1
anurath <anurathwaghmode2018@gmail.com>
2025-06-17 23:47:10 +0530

    Created Country class file to achive toString method overriding

---
+++ PrinciplesOfOOP/principlesOfOOp/src/objectClass/Country.java
@@ -0,0 +1,18 @@
+package objectClass;
+
+public class Country {
+	String name;
+	String capital;
+	String hos;
+	int states;
+	
+	Country(){}
+	
+	Country(String name,String capital,String hos,int states)
+	{
+		this.name = name;
+		this.capital = capital;
+		this.hos = hos;
+		this.states = states;
+	}
+}
