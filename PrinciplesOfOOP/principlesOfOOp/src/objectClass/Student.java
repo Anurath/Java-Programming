0d9c9f502dbbd6bf313d244741e7831f2f823fce
anurath <anurathwaghmode2018@gmail.com>
2025-06-17 23:37:31 +0530

    Student file is added for the object class

---
+++ PrinciplesOfOOP/principlesOfOOp/src/objectClass/Student.java
@@ -0,0 +1,18 @@
+package objectClass;
+
+public class Student {
+	String name;
+	String college;
+	int rollNo;
+	String degree;
+	
+	Student(){}
+	
+	Student(String name, String college,int rollNo,String degree)
+	{
+		this.name = name;
+		this.college = college;
+		this.rollNo = rollNo;
+		this.degree = degree;
+	}
+}
