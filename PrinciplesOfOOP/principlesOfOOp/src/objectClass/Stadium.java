dc27b231a4077d09e002bbe9523baca413148bd6
anurath <anurathwaghmode2018@gmail.com>
2025-06-19 12:42:41 +0530

    hashCode method added

--- PrinciplesOfOOP/principlesOfOOp/src/objectClass/Stadium.java
+++ PrinciplesOfOOP/principlesOfOOp/src/objectClass/Stadium.java
@@ -1,5 +1,30 @@
 package objectClass;
 
 public class Stadium {
-
+	String name;
+	int capacity;
+	String game;
+	int year;
+	String homeTeam;
+	
+	Stadium(){}
+	
+	Stadium(String name,int capacity,String game,int year,String homeTeam)
+	{
+		this.name = name;
+		this.capacity = capacity;
+		this.game = game;
+		this.year = year;
+		this.homeTeam = homeTeam;
+	}
+	
+//	public String toString()
+//	{
+//		return "Name: "+" ,Capacity: "+capacity+" ,Game: "+game+",Year: "+year+" ,Home Team: "+homeTeam;
+//	}
+	
+	public int hashCode()
+	{
+		return capacity+year;
+	}
 }
--- PrinciplesOfOOP/principlesOfOOp/src/objectClass/StadiumDriver.java
+++ PrinciplesOfOOP/principlesOfOOp/src/objectClass/StadiumDriver.java
@@ -1,5 +1,15 @@
 package objectClass;
 
 public class StadiumDriver {
-
+	public static void main(String[] args)
+	{
+		Stadium s1 = new Stadium("Brebon",30000,"Cricket",1940,"Mumbai");
+		Stadium s2 = new Stadium("Brebon",30000,"Cricket",1940,"Mumbai");
+		System.out.println(s1.toString());
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		
+		
+	}
 }
