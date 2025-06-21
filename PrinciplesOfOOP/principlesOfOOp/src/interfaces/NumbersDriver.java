51e341f5ea9dfa58d8779299cf4ed1d3c52a9e63
anurath <anurathwaghmode2018@gmail.com>
2025-06-21 23:05:59 +0530

    Driver file added for Interface

---
+++ PrinciplesOfOOP/principlesOfOOp/src/interfaces/NumbersDriver.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public class NumbersDriver {
+	public static void main(String[] args)
+	{
+		Numbers n1 = new NumberImp();
+		
+		System.out.println(n1.isPrime(12));
+		System.out.println(n1.prevPrimeNumber(34));
+		System.out.println(n1.nextPrime(45));
+	}
+}
