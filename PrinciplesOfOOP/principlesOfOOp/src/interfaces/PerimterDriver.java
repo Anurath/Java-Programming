fb44fe63489a8776ad1d1ef4c1ecbf278b023679
anurath <anurathwaghmode2018@gmail.com>
2025-06-22 23:24:35 +0530

    Method body is created in the Implements file

--- PrinciplesOfOOP/principlesOfOOp/src/interfaces/PerimterDriver.java
+++ PrinciplesOfOOP/principlesOfOOp/src/interfaces/PerimterDriver.java
@@ -8,5 +8,6 @@ public class PerimterDriver {
 		System.out.println(p1.triangle(10, 20, 30));
 		System.out.println(p1.square(10));
 		System.out.println(p1.ractangle(10, 20));
+		System.out.println(p1.trapazium(23, 34));
 	}
 }
