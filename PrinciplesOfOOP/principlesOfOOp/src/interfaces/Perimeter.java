0793219dacee462dfd0bb372d9b226f2425aaa91
anurath <anurathwaghmode2018@gmail.com>
2025-06-22 23:09:09 +0530

    New file added for Interface

---
+++ PrinciplesOfOOP/principlesOfOOp/src/interfaces/Perimeter.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public interface Perimeter {
+	double circle(double rad);
+	
+	double ractangle(double l,double b);
+	
+	double triangle(double s1,double s2,double s3);
+	
+	double square(double s);
+	
+}
