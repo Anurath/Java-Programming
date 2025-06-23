0790f1eb01787c0c68ccd105599bc5c1960f3dc1
anurath <anurathwaghmode2018@gmail.com>
2025-06-23 23:17:29 +0530

    Concrete method designed in the class file which is implementing Interface Instragram

--- PrinciplesOfOOP/principlesOfOOp/src/multipleInheritance/InstragramDriver.java
+++ PrinciplesOfOOP/principlesOfOOp/src/multipleInheritance/InstragramDriver.java
@@ -3,6 +3,10 @@ package multipleInheritance;
 public class InstragramDriver {
 	public static void main(String[] args)
 	{
-		Instagram i1 = new InstragramImp()
+		Instagram i1 = new InstragramImp1();
+		
+		i1.post("ram","Hello Kaisa hai!!!");
+		
+		i1.like();
 	}
 }
