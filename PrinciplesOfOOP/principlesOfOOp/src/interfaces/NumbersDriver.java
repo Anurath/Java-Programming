f729d3da9776a32397399180416423d1cb8071f8
anurath <anurathwaghmode2018@gmail.com>
2025-06-30 22:43:27 +0530

    Files modified

--- PrinciplesOfOOP/principlesOfOOp/src/interfaces/NumbersDriver.java
+++ PrinciplesOfOOP/principlesOfOOp/src/interfaces/NumbersDriver.java
@@ -8,6 +8,6 @@ public class NumbersDriver {
 		System.out.println(n1.isPrime(12));
 		System.out.println(n1.prevPrimeNumber(34));
 		System.out.println(n1.nextPrime(45));
-		System.out.println(n1.fact(12));
+		System.out.println(n1.sum(12));
 	}
 }
