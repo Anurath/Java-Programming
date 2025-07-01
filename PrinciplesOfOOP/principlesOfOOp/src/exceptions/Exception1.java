1ee23a9fcab4204cce329979d9a769a974da4df4
anurath <anurathwaghmode2018@gmail.com>
2025-07-01 11:15:53 +0530

    Files added for the exception handling

---
+++ PrinciplesOfOOP/principlesOfOOp/src/exceptions/Exception1.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class Exception1 {
+
+	public void division(int n,int d)
+	{
+		System.out.println(n/d);
+	}
+}
---
+++ PrinciplesOfOOP/principlesOfOOp/src/exceptions/Exception1Driver.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class Exception1Driver {
+	public static void main(String[] args)
+	{
+		Exception1 e1 = new Exception1();
+		
+		e1.division(12, 0);
+	}
+}
