6d30d8aa20117021652fd2edbb7579385221e3d3
anurath <anurathwaghmode2018@gmail.com>
2025-07-01 11:23:53 +0530

    try with multiple catch blocks

---
+++ PrinciplesOfOOP/principlesOfOOp/src/exceptions/Exception2.java
@@ -0,0 +1,23 @@
+package exceptions;
+
+public class Exception2 {
+
+	public void division(int n,int d)
+	{
+		try {
+			System.out.println(n/d);
+		}
+		catch(NullPointerException e)
+		{
+			System.out.println("From null ponter exception");
+		}
+		catch(ClassCastException e)
+		{
+			System.out.println("From class cast exceptoin");
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("From arithmetic exception");
+		}
+	}
+}
