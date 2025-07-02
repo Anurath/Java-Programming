d32cfe283efd32372fedf5538f3d41fa04143936
anurath <anurathwaghmode2018@gmail.com>
2025-07-02 23:26:57 +0530

    Method added for multiple catch block

--- PrinciplesOfOOP/principlesOfOOp/src/exceptions/Test.java
+++ PrinciplesOfOOP/principlesOfOOp/src/exceptions/Test.java
@@ -2,4 +2,25 @@ package exceptions;
 
 public class Test {
 
+	public void division(int a , int b)
+	{
+		try
+		{
+			System.out.println(a/b);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+		
+		catch(NullPointerException e)
+		{
+			System.out.println(e);
+		}
+		
+		catch(ClassCastException e)
+		{
+			System.out.println(e);
+		}
+	}
 }
