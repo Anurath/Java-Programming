4ff194ca7e24b0370c2be756f9be879d4507ffc2
anurath <anurathwaghmode2018@gmail.com>
2025-07-04 12:48:48 +0530

    File added for wrapper class

--- PrinciplesOfOOP/principlesOfOOp/src/wrapperClass/Test.java
+++ PrinciplesOfOOP/principlesOfOOp/src/wrapperClass/Test.java
@@ -1,5 +1,16 @@
 package wrapperClass;
 
 public class Test {
-
+	public static void main(String[] args)
+	{
+		int n1 = 10;
+		int n2 = 10;
+		
+		Integer i1 = new Integer(n1);
+		Integer i2 = new Integer(n2);
+		
+		System.out.println(i1.toString());
+		System.out.println(i2.toString());
+		System.out.println(i1.equals(i2));
+	}
 }
