58b72c47646db817be43ebe1c46d578b28684d5f
anurath <anurathwaghmode2018@gmail.com>
2025-07-04 12:58:59 +0530

    File added for wrapper class

--- PrinciplesOfOOP/principlesOfOOp/src/wrapperClass/Test.java
+++ PrinciplesOfOOP/principlesOfOOp/src/wrapperClass/Test.java
@@ -9,6 +9,8 @@ public class Test {
 		char ch1 = 'a';
 		char ch2 = 'a';
 		
+		long num1 = 10;
+		long num2 = 10;
 		
 		Integer i1 = new Integer(n1);
 		Integer i2 = new Integer(n2);
@@ -28,5 +30,14 @@ public class Test {
 		System.out.println(c1.toString());
 		System.out.println(c2.toString());
 		
+		System.out.println("----------------------------------");
+		
+		Long l1 = new Long(num1);
+		Long l2 = new Long(num2);
+		
+		System.out.println(l1.equals(l2));
+		System.out.println(l1.toString());
+		System.out.println(l2.toString());
+		System.out.println(l1 == l2);
 	}
 }
