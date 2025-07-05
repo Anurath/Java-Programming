174c13b5b13aac985fdb469b96279f734556d9d2
anurath <anurathwaghmode2018@gmail.com>
2025-07-05 12:22:29 +0530

    NumberFormatException

---
+++ PrinciplesOfOOP/principlesOfOOp/src/wrapperClass/NumberFormatException.java
@@ -0,0 +1,24 @@
+package wrapperClass;
+
+public class NumberFormatException {
+	
+	public static void main(String[] args)
+	{
+		String s1 = "100sd";
+		String s2 = "";
+//		byte num1 = Byte.parseByte(num1);  //NumberFormatException
+		
+//		short num2 = Short.parseShort(s1);  //NumberFormatException
+		
+//		int num3 = Integer.parseInt(s1);  //NumberFormatException
+		
+//		long num4 = Long.parseLong(s1);  //NumberFormatException
+		
+//		float num5 = Float.parseFloat(s1);  //NumberFormatException
+		
+//		double num6 = Double.parseDouble(s1);  //NumberFormatException
+		
+		char ch = s2.charAt(0);
+		
+	}
+}
