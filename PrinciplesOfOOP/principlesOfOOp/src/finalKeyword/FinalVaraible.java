b0d37a548f99e97a3c5d90a6329a5674efffa0ce
anurath <anurathwaghmode2018@gmail.com>
2025-07-06 22:15:06 +0530

    Local final variable file added

---
+++ PrinciplesOfOOP/principlesOfOOp/src/finalKeyword/FinalVaraible.java
@@ -0,0 +1,14 @@
+package finalKeyword;
+
+public class FinalVaraible {
+	
+	// Final Local variable
+	public static void main(String[] args)
+	{
+		final int a;
+		a=10;
+		System.out.println(a);
+		
+//		a=20;               // we can not reinitialize variable again in for local final variable
+	}
+}
