91c5fc7e59a3905e736511920cea52da7a171890
anurath <anurathwaghmode2018@gmail.com>
2025-07-07 23:44:00 +0530

    Element insertetion in an array

---
+++ PrinciplesOfOOP/principlesOfOOp/src/array/Demo1.java
@@ -0,0 +1,13 @@
+package array;
+
+public class Demo1 {
+
+	public static void main(String[] args)
+	{
+		int[] arr1 = new int[4];
+		arr1[0] = 10;
+		arr1[1] = 20;
+		arr1[2] = 30;
+		arr1[3] = 40;
+	}
+}
