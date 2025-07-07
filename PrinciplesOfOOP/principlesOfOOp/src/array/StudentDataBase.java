0d9f5c7efcc88e867e4169dfa9c11db0814c0d24
anurath <anurathwaghmode2018@gmail.com>
2025-07-07 23:55:35 +0530

    Created multiple student object and stored in the db array of type Student

--- PrinciplesOfOOP/principlesOfOOp/src/array/StudentDataBase.java
+++ PrinciplesOfOOP/principlesOfOOp/src/array/StudentDataBase.java
@@ -4,11 +4,12 @@ public class StudentDataBase {
 
 	public static void main(String[] args)
 	{
-		Student[] db = new Student[3];
+		Student[] db = new Student[4];
 		
 		db[0] = new Student("Anurath","PK",22,4156);
 		db[1] = new Student("Kunal","PK",21,4139);
 		db[2] = new Student("Sanket","PK",22,4106);
+		db[3] = new Student("Ram","PK",23,4149);
 		
 		for(int i=0;i<db.length;i++)
 		{
