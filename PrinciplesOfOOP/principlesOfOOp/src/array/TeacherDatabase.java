a15d4f6ada3e57517f98213cc8dcc07b4ae82deb
anurath <anurathwaghmode2018@gmail.com>
2025-07-08 13:47:59 +0530

    TeacherDatabase class modified

--- PrinciplesOfOOP/principlesOfOOp/src/array/TeacherDatabase.java
+++ PrinciplesOfOOP/principlesOfOOp/src/array/TeacherDatabase.java
@@ -4,11 +4,11 @@ public class TeacherDatabase {
 
 	public static void main(String[] args)
 	{
+		Scanner sc = new Scanner(System.in);
 		Teacher[] db = new Teacher[5];
 		
 		for(int i=0;i<db.length;i++)
 		{
-			Scanner sc = new Scanner(System.in);
 			System.out.println("Enter the name of teacher: ");
 			String name = sc.nextLine();
 			System.out.println("Enter the college name: ");
@@ -20,6 +20,20 @@ public class TeacherDatabase {
 			String subject = sc.nextLine();
 			
 			db[i] = new Teacher(name,clg,age,subject,phno);
+		}
+		System.out.println("Do you want to see Teacher Information:(Yes-1,No-0)");
+		int show = sc.nextInt();
+		
+		if(show==1)
+		{
+			for(int i=0;i<db.length;i++)
+			{
+				System.out.println(db[i]);
+			}
+		}
+		else
+		{
+			System.out.println("Thank you...");
 		}
 	}
 }
