50063889a4bfc1e20528252feb3a2d5e934f57ae
anurath <anurathwaghmode2018@gmail.com>
2025-07-08 13:36:45 +0530

    Teacher class added for teacher database

---
+++ PrinciplesOfOOP/principlesOfOOp/src/array/Teacher.java
@@ -0,0 +1,21 @@
+package array;
+
+public class Teacher {
+
+	String name;
+	String clg;
+	int age;
+	String subject;
+	long phno;
+	
+	Teacher(){}
+	
+	Teacher(String name,String clg,int age,String subject,long phno)
+	{
+		this.name = name;
+		this.clg  = clg;
+		this.age = age;
+		this.subject = subject;
+		this.phno = phno;
+	}
+}
