0916de04c62c7c0b3f1073c3146b61a4fe4c6a12
anurath <anurathwaghmode2018@gmail.com>
2025-07-11 13:27:27 +0530

    Missing element file completed

--- PrinciplesOfOOP/principlesOfOOp/src/array/MissingElement.java
+++ PrinciplesOfOOP/principlesOfOOp/src/array/MissingElement.java
@@ -1,20 +1,25 @@
 package array;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class MissingElement {
 
 	public static void main(String[] args)
 	{
-		int[] arr = {8,7,3,4,5,6,1,2};
+		int[] arr = {1,2,3,4,5,7,8};
+		
+		int missNum = missingElement(arr);
+		System.out.println(missNum);
 		
 	}
 	public static int missingElement(int[] arr)
 	{
-		int j=0;
-		for(int i=0;i<arr.length;i++)
+		for(int i=0,j=1;i<arr.length;i++,j++)
 		{
 			if(arr[i]!=j)
 			{
-				return j;
+				return i+1;
 			}
 		}
 		return -1;
