01f0745c612513cec1fabfe2265a3abde94d1711
anurath <anurathwaghmode2018@gmail.com>
2025-07-11 13:43:50 +0530

    Make Negative

---
+++ PrinciplesOfOOP/principlesOfOOp/src/array/MakeNegative.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Arrays;
+
+public class MakeNegative {
+
+	public static int[] makeNegative(int[] arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]>0)
+			{
+				arr[i]=arr[i]*(-1);
+			}
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] arr = {1,2,3,4,3,-5,-6,-3,2,5,6,7-1};
+		System.out.println(Arrays.toString(makeNegative(arr)));
+	}
+}
