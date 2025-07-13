612eb78f0d569924e167d1eb6265267165b35099
anurath <anurathwaghmode2018@gmail.com>
2025-07-13 11:25:16 +0530

    File modified

--- PrinciplesOfOOP/principlesOfOOp/src/array/NegativePositivePush.java
+++ PrinciplesOfOOP/principlesOfOOp/src/array/NegativePositivePush.java
@@ -31,7 +31,7 @@ public class NegativePositivePush {
 	
 	public static void main(String[] args)
 	{
-		int[] arr= {-3,1,2,-3,4,-6,-4,3,6};
+		int[] arr= {-3,1,-1,4,-5,8,-4,2,-3,4,-6,-4,3,6};
 		negPosPush(arr);
 		System.out.println(Arrays.toString(arr));
 	}
