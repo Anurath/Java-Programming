79e44ba0fbf679d8dd882020cf83e32c27c03a37
anurath <anurathwaghmode2018@gmail.com>
2025-07-14 23:40:27 +0530

    File modified

--- PrinciplesOfOOP/principlesOfOOp/src/array/ArrayInsertion.java
+++ PrinciplesOfOOP/principlesOfOOp/src/array/ArrayInsertion.java
@@ -1,11 +1,34 @@
 package array;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class ArrayInsertion {
 
-	public static int[] arrayInsertion(int[] a,int[] b,int start,int end)
-	
+	public static int[] arrayInsertion(int[] a,int[] b,int idx)
+	{
+		if(idx<a.length&&idx>=0)
+		{
+			int j=0;
+			int[] ans = new int[a.length+b.length];
+			for(int i=0;i<ans.length;i++)
+			{
+				if(i<idx)
+				{
+					ans[i]=a[i];
+				}
+				else if(i<(idx+b.length))
+				{
+					ans[i]=b[j++];
+				}
+				else {
+					ans[i]=a[i-b.length];
+				}
+			}
+			return ans;
+		}
+		return null;
+	}
 	
 	public static void main(String[] args)
 	{
