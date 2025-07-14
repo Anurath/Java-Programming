7dd881d37bbb489c1dd9811f7e31b7172b7a0abd
anurath <anurathwaghmode2018@gmail.com>
2025-07-14 23:58:47 +0530

    File modified

--- PrinciplesOfOOP/principlesOfOOp/src/array/arrayInsertionInRange.java
+++ PrinciplesOfOOP/principlesOfOOp/src/array/arrayInsertionInRange.java
@@ -1,5 +1,38 @@
 package array;
 
+import java.util.Arrays;
+
 public class arrayInsertionInRange {
 
+	public static int[] rangeInsertion(int[] a,int[] b,int start,int end)
+	{
+		int j=0;
+		if(start>=0&&end<a.length&&start<end)
+		{
+			int[] ans = new int[a.length+b.length];
+			
+			for(int i=0;i<ans.length;i++)
+			{
+				if(i<start)
+				{
+					ans[i]=a[i];
+				}
+				else if(i<(start+b.length))
+				{
+					ans[i]=b[j++];
+				}
+				else {
+					ans[i]=a[i-b.length];
+				}
+			}
+			return ans;
+		}
+		return null;
+	}
+	public static void main(String[] args)
+	{
+		int[] a = {1,2,3,4,5,6,7};
+		int[] b={10,20,30};
+		System.out.println(Arrays.toString(rangeInsertion(a, b, 2, 3)));
+	}
 }
