216dbb16daff382ee7447b6b1723492a8330644e
anurath <anurathwaghmode2018@gmail.com>
2025-07-17 12:22:54 +0530

    Linear Search file added

--- DSA/DSA_JAVA/src/searching/LinearSearch.java
+++ DSA/DSA_JAVA/src/searching/LinearSearch.java
@@ -1,5 +1,24 @@
 package searching;
 
+
+
 public class LinearSearch {
 
+	public static int linearSearch(int[] arr,int ele)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==ele)
+				return i;
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] arr = {1,2,3,4,5,6,7,8,12,9,1,2,3,4,5,6,7,8};
+		
+		int ans = linearSearch(arr, 12);
+		System.out.println(ans);
+	}
 }
