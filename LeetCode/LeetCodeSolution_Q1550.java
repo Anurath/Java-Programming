bc7d00fd938514b122b01fd00669e0fe07813c7e
anurath <anurathwaghmode2018@gmail.com>
2025-07-21 22:37:15 +0530

    Leet code solution added for Q.1550

---
+++ LeetCode/LeetCodeSolution_Q1550.java
@@ -0,0 +1,20 @@
+class LeetCodeSolution_Q1550{
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int cnt=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                cnt++;
+            }
+            else{
+                cnt=0;
+            }
+            if(cnt>=3)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
