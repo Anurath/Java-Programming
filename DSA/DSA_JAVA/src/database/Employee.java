b05db6b9b293ef1283e990bc6009f9c9ebf8ffa4
anurath <anurathwaghmode2018@gmail.com>
2025-07-22 10:58:27 +0530

    Structuring database

---
+++ DSA/DSA_JAVA/src/database/Employee.java
@@ -0,0 +1,29 @@
+package database;
+
+public class Employee {
+
+	String name;
+	int empid;
+	int deptNo;
+	int managerNo;
+	String hireDate;
+	double sal;
+	double comm;
+	String job;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Employee(String name,int empid,int deptNo,int managerNo,String hireDate,double sal,double comm,String job)
+	{
+		this.name = name;
+		this.empid = empid;
+		this.deptNo = deptNo;
+		this.managerNo = managerNo;
+		this.hireDate = hireDate;
+		this.sal = sal;
+		this.comm = comm;
+		this.job = job;
+	}
+}
