9b5ddab7e825cd1c8683c0f812b49da2c5846f1e
anurath <anurathwaghmode2018@gmail.com>
2025-07-26 11:44:25 +0530

    sum in 2D array

--- DSA/DSA_JAVA/src/twoDimArray/SumOfTwoDim.java
+++ DSA/DSA_JAVA/src/twoDimArray/SumOfTwoDim.java
@@ -1,11 +1,17 @@
 package twoDimArray;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class SumOfTwoDim {
 
 	public static void main(String[] args)
 	{
-		int sum = 0;
 		int[][] nums = {{1,2,3,4},{2,3,4,5},{6,7,8,100}};
+	}
+	public static void sum(int[][] nums)
+	{
+		int sum = 0;
 		
 		for (int i = 0; i < nums.length; i++) {
 			for (int j = 0; j < nums[i].length; j++) {
@@ -13,5 +19,55 @@ public class SumOfTwoDim {
 			}
 		}
 		System.out.println(sum);
+	}
+	
+	public static void diagonalSum(int[][] nums)
+	{
+		int sum = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			 for (int j = 0; j < nums[i].length; j++) {
+				if(i==j)
+				{
+					sum+=nums[i][j];
+				}
+			}
+		}
+		System.out.println(sum);
+	}
+	
+	public static void sumOfTwoTwoDimArray(int[][] a , int[][]b)
+	{
+		boolean flag = true;
+		if(a.length==b.length)
+		{
+			for(int i=0;i<a.length;i++)
+			{
+				if(a[i].length!=b.length))
+				{
+					flag = false;
+				}
+			}
+		}
+		if(flag)
+		{
+			int[][] c = new int[a.length][a[0].length];
+			
+			for(int i=0;i<a.length;i++)
+			{
+				for(int j=0;j<a[0].length;j++)
+				{
+					c[i][j] = a[i][j]+b[i][j];
+				}
+			}
+			
+			for(int i=0;i<c.length;i++)
+			{
+				System.out.println(Arrays.toString(c[i]));
+			}
+		}
+		else {
+			System.out.println("Arrays are not symmetrical");
+		}
 	}
 }
