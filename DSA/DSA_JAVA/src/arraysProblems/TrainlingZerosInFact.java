b5b978176b7d1ce2b9c5e2cdaa5e61c171144406
anurath <anurathwaghmode2018@gmail.com>
2025-07-30 23:36:02 +0530

    Trailing zeros in factorail file added

---
+++ DSA/DSA_JAVA/src/arraysProblems/TrainlingZerosInFact.java
@@ -0,0 +1,20 @@
+package arraysProblems;
+
+public class TrainlingZerosInFact {
+
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public static factorial(int num)
+	{
+		int fact = 1;
+		while(num!=0)
+		{
+			fact*=num;
+			num--;
+		}
+		return fact;
+	}
+}
