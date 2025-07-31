bf5214af63369858fcceaabf7860b34458f85042
anurath <anurathwaghmode2018@gmail.com>
2025-07-31 14:00:33 +0530

    Leet code solution added for Q.1572

---
+++ LeetCode/LeetCodeSolution_Q1572.java
@@ -0,0 +1,16 @@
+class LeetCodeSolution_Q1572{
+    public int diagonalSum(int[][] mat) {
+       int sum = 0;
+       for(int i=0;i<mat.length;i++)
+       {
+            for(int j=0;j<mat[i].length;j++)
+            {
+                if(i==j||mat[i].length-i-1==j)
+                {
+                    sum+=mat[i][j];
+                }
+            }
+       } 
+       return sum;
+    }
+}
