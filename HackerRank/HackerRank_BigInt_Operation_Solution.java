887b26fbfb46c5e908b424370899fdc0ac24e146
anurath <anurathwaghmode2018@gmail.com>
2025-08-09 10:47:40 +0530

    Hacker rank BigInteger operation solution added

---
+++ HackerRank/HackerRank_BigInt_Operation_Solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class HackerRank_BigInt_Operation_Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String num1 = sc.next();
+        String num2 = sc.next();
+        
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+        
+        
+        System.out.println(n1.add(n2));
+        System.out.println(n1.multiply(n2));
+    }
+}
