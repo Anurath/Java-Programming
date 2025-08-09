100a81f13df07f59d90c409e3bfdd0678a1d90dc
anurath <anurathwaghmode2018@gmail.com>
2025-08-09 16:28:28 +0530

    split array

---
+++ DSA/DSA_JAVA/src/string/SplitByChar.java
@@ -0,0 +1,49 @@
+package string;
+
+import java.util.Arrays;
+
+public class SplitByChar {
+
+	public static void main(String[] args) {
+		String str = "I love India.";
+		System.out.println(Arrays.toString(split(str, 'o')));
+	}
+	
+	public static String[] split(String str, char ch)
+	{
+		int cnt=0;
+		for(int i=1;i<str.length()-1;i++)
+		{
+			if(str.charAt(i)==ch)
+			{
+				cnt++;
+			}
+		}
+		if(cnt>0)
+		{
+			int idx =0;
+			String[] ans = new String[cnt+1];
+			
+			 for(int i=0;i<str.length();i++)
+			 {
+				 String temp="";
+				 while(i<str.length() && str.charAt(i)!=ch)
+				 {
+					 temp+=str.charAt(i);
+					 i++;
+				 }
+				 if(temp.length()>0)
+				 {
+					 if(idx<=cnt)
+					 {
+						 ans[idx++]=temp;
+					 }
+					 temp="";
+				 }
+			 }
+			 return ans;
+		}
+		return null;
+	}
+	
+}
