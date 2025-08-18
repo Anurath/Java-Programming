55fe55bf518e877a7d8b1f0c64e29a3c68c1d69e
anurath <anurathwaghmode2018@gmail.com>
2025-08-19 00:39:56 +0530

    Leet code solution for Q.2255

---
+++ LeetCode/LeetCodeSolution_Q2255.java
@@ -0,0 +1,13 @@
+class LeetCodeSolution_Q2255{
+    public int countPrefixes(String[] words, String s) {
+        int cnt = 0 ;
+        for(int i=0;i<words.length;i++)
+        {
+            if(s.startsWith(words[i]))
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
