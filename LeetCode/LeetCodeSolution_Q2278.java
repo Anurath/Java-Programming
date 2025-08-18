da5c31b42f05cec2364599fa33fc15b7642015c1
anurath <anurathwaghmode2018@gmail.com>
2025-08-19 00:40:17 +0530

    Leet code solution for Q.2278

---
+++ LeetCode/LeetCodeSolution_Q2278.java
@@ -0,0 +1,13 @@
+class LeetCodeSolution_Q2278{
+    public int percentageLetter(String s, char letter) {
+        int cnt = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==letter)
+            {
+                cnt++;
+            }
+        }
+        return cnt*100/s.length();
+    }
+}
