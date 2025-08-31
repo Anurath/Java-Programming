af4a641d86b1292dc71b997b99a22a23584ee6ff
anurath <anurathwaghmode2018@gmail.com>
2025-08-31 22:48:10 +0530

    Leet code solution added for Q.1455

---
+++ LeetCode/LeetCodeSolution_Q1455.java
@@ -0,0 +1,14 @@
+class LeetCodeSolution_Q1455 {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] str = sentence.split(" ");
+        
+        for(int i=0;i<str.length;i++)
+        {
+            if(str[i].startsWith(searchWord))
+            {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
