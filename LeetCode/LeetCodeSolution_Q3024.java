7dc719b9de2e8ae9da76fb0c9f83a769cab3da3b
anurath <anurathwaghmode2018@gmail.com>
2025-09-05 23:16:11 +0530

    Leet code solution added for Q3024

---
+++ LeetCode/LeetCodeSolution_Q3024.java
@@ -0,0 +1,25 @@
+class LeetCodeSolution_Q3024 {
+    public String triangleType(int[] nums) {
+        if((nums[0]==nums[1])&& nums[1]==nums[2])
+        {
+            return "equilateral";
+        }
+        else if((nums[0]==nums[1])||(nums[0]==nums[2])|| (nums[1]==nums[2]))
+        {
+            if(nums[0]+nums[1]>nums[2]&&nums[0]+nums[2]>nums[1]&&nums[2]+nums[1]>nums[0]){
+                 return "isosceles";
+            }
+            else{
+                return "none";
+            }
+        }
+        else{
+            if(nums[0]+nums[1]>nums[2]&&nums[0]+nums[2]>nums[1]&&nums[2]+nums[1]>nums[0]){
+                 return "scalene";
+            }
+            else{
+                return "none";
+            }
+        }
+    }
+}
