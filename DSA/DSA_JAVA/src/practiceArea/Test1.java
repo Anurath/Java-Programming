664cdd088fedcb6b20c2f9a8e9aff05013c9bf4d
anurath <anurathwaghmode2018@gmail.com>
2025-09-11 10:43:47 +0530

    File added for Static final varaible

---
+++ DSA/DSA_JAVA/src/practiceArea/Test1.java
@@ -0,0 +1,20 @@
+package practiceArea;
+
+public class Test1 {
+	
+	static final double PI;
+	 static{
+		PI=200;
+	}
+	 
+	 
+	{
+		age =200;
+	}
+	
+	final int age ;
+	
+	public Test1(int val) {
+	   
+	}
+}
