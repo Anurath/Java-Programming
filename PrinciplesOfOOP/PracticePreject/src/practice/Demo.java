d7fd13eb40bc223b736d6e9eee3510320b3a8f07
anurath <anurathwaghmode2018@gmail.com>
2025-09-29 23:19:32 +0530

    Question number added

--- PrinciplesOfOOP/PracticePreject/src/practice/Demo.java
+++ PrinciplesOfOOP/PracticePreject/src/practice/Demo.java
@@ -1,40 +1,16 @@
 package practice;
+import java.util.*;
 import java.util.Collections;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Student> s1 = new ArrayList<Student>();
+		List<String> list1 = List.of("Ram","Lakhan","Sham");
 		
-		s1.add(new Student(1,"Anurath",23,"PK",2025));
-		s1.add(new Student(1,"Kunal",22,"SP",2025));
-		s1.add(new Student(1,"Anket",22,"BK",2025));
-		s1.add(new Student(1,"Sanket",21,"DY",2025));
-		s1.add(new Student(1,"Ram",20,"FC",2025));
-		s1.add(new Student(1,"Anurath",23,"PK",2023));
-		s1.add(new Student(1,"Anurath",29,"PK",2025));
-		s1.add(new Student(1,"Anurath",23,"PK",2024));
-		s1.add(new Student(1,"Karan",23,"MC",2025));
-		s1.add(new Student(1,"Harsh",25,"JC",2025));
-		s1.add(new Student(1,"Mohit",25,"PK",2025));
-		
-//		System.out.println(s1);
-		
-//		Collections.sort(s1);
-		
-//		System.out.println(s1);
-		
-		Collections.reverse(s1);
-		
-		System.out.println(s1);
-		
-		System.out.println(Collections.max(s1));
-		
-		System.out.println(Collections.min(s1));
-		
-		System.out.println(Collections.frequency(s1,new Student(1,"Anurath",23,"PK",2025) ));
+		list1.set(0, "Raghav");
 	}
 }
