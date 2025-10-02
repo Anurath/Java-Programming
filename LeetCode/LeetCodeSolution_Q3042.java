c894f5e582ae7fbd3a808ee19dcfbf82859e4ec3
anurath <anurathwaghmode2018@gmail.com>
2025-10-02 23:43:30 +0530

    Leet code solution for Q.3042

---
+++ LeetCode/LeetCodeSolution_Q3042.java
@@ -0,0 +1,16 @@
+class LeetCodeSolution_Q3042 {
+    public int countPrefixSuffixPairs(String[] words) {
+        int cnt = 0 ;
+        for(int i=0;i<words.length;i++)
+        {
+            for(int j=i+1;j<words.length;j++)
+            {
+                if(words[j].startsWith(words[i])&&words[j].endsWith(words[i]))
+                {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
